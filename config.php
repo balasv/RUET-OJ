f731ae23a33d4bae1cdbb6a120114b648c7a2ba5
Ashadullah Shawon <shawonashadullah@gmail.com>
2017-10-11 17:38:28 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$db="reg";
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("databaseuser");
+$pass = getenv("databasepassword");
+$db =   getenv("databasename");
 
 $con=mysqli_connect($host,$user,$pass,$db);
 
