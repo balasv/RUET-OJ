a76a5d424934077dda144f86e55e0dd82de707e2
Ashadullah Shawon <shawonashadullah@gmail.com>
2017-10-29 04:02:29 +0600

    Update index.php

--- mail/index.php
+++ mail/index.php
@@ -9,11 +9,11 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'ruetoj@gmail.com';                 // SMTP username
-$mail->Password = '';                           // SMTP password
+$mail->Password = 'shawonruet';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->setFrom('shawon448@gmail.com', 'Mailer');
+$mail->setFrom('ruetoj@gmail.com', 'Mailer');
 $mail->addAddress('shawonashadullah@gmail.com', 'User');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
